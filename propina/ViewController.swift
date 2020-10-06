7402ced9dec0066b5f46e707fcbdc6933e37e3d4
Alumno <cesarrgomez@hotmail.com>
2020-10-06 15:20:21 -0700

    propina

--- propina/ViewController.swift
+++ propina/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblPorcentaje: UILabel!
+    @IBOutlet weak var sldPorcentaje: UISlider!
+    @IBOutlet weak var lblTotalConPropina: UILabel!
+    @IBOutlet weak var txtCuenta: UITextField!
+    @IBAction func doChangePorcentaje(_ sender: Any) {
+        lblPorcentaje.text = "\(Int(sldPorcentaje.value))%"
+        var cuenta = 0.0
+        if txtCuenta.text != nil && txtCuenta.text != "" {
+            cuenta = Double(txtCuenta.text!)!
+        }
+        let cuentaConPropina = cuenta + cuenta * (Double(sldPorcentaje.value)/100)
+        let strCuentaConPropina = String(format: "%.2f", cuentaConPropina)
+        lblTotalConPropina.text = "$\(strCuentaConPropina)"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
